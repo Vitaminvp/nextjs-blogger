b32ffe663a04fee7f0f92451370d5cf8bbb3a203
Vitalii Vitaminenko <vitamin@ukr.net>
2019-06-14 12:09:20 +0300

    first commit

---
+++ pages/about.js
@@ -0,0 +1,4 @@
+const About = ({title = "About!!"}) => <div>
+    <h2>{title}</h2>
+</div>;
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,4 @@
+const Index = ({title = "Hello World!!"}) => <div>
+    <h2>{title}</h2>
+</div>;
+export default Index;
